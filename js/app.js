2efef8eab04e922ce17e83304717996818a340f7
vishal kumar <vg780538@gmail.com>
2022-05-31 02:44:03 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+const getElement = (selector) => {
+  //WHAT DOES CONST MEANS 
+//=> WHAT DOES THIS WORK
+//QUERY SELECTORJ?
+//$ WHAT IT MEANS
+
+  const element = document.querySelector(selector);
+ if(element) return element
+ throw Error(`please double check your class names, there is no ${selector} class`
+ )
+}
+ 
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+ 
+ navBtnDOM.addEventListener('click', () => {
+   links.classList.toggle('show-links')
+ })
+
+
+ const date = getElement('#date');
+ const currentYear = new Date().getFullYear()
+ date.textContent = currentYear
